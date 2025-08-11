946170355232ab1c3c267e51cbde2c705f3a3f17
ValentinaNarinoCh <v.narino@uniandes.edu.co>
2025-08-10 19:17:46 -0500

    Actualizacion funcion cargue datos

--- app.py
+++ app.py
@@ -22,10 +22,10 @@ app.config.suppress_callback_exceptions = True
 # Load data from csv
 def load_data():
     # To do: Completar la función
-    data=pd.read_csv('datos_energia.csv')
-    data['time'] = pd.to_datetime(data['time'])
-    data.set_index('time', inplace=True)
-    return data
+    data1=pd.read_csv('datos_energia.csv')
+    data1['time'] = pd.to_datetime(data1['time'])
+    data1.set_index('time', inplace=True)
+    return data1
 
 # Cargar datos
 data = load_data()
